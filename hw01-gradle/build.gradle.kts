9322c47a563168a9d96929617167a9d8d81ee048
devbiirds <vadikoff57@gmail.com>
2024-07-15 22:12:47 +0300

    fix

--- hw01-gradle/build.gradle.kts
+++ hw01-gradle/build.gradle.kts
@@ -14,7 +14,7 @@ tasks {
         archiveVersion.set("0.1")
         archiveClassifier.set("")
         manifest {
-            attributes(mapOf("HelloOtus" to "ru.otus.HelloOtus"))
+            attributes(mapOf("Main-Class" to "ru.otus.HelloOtus"))
         }
     }
 
